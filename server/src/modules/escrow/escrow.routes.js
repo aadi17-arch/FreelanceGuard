0b8d144be9e66caedbe8f96a8d9c63998a99c886
aadi17-arch <facc6982@gmail.com>
2026-05-06 02:34:41 +0530

    feat(escrow-routes): expose add-funds and withdraw wallet API endpoints

--- server/src/modules/escrow/escrow.routes.js
+++ server/src/modules/escrow/escrow.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { depositToEscrow, getUserContracts, releaseFunds } from "./escrow.controller.js";
+import { depositToEscrow, getUserContracts, releaseFunds, addFundsToWallet, withdrawFundsFromWallet } from "./escrow.controller.js";
 import authMiddleware from "../auth/auth.middleware.js";
 
 
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("/", authMiddleware, getUserContracts);
 router.post("/release/:contractId", authMiddleware, releaseFunds);
 router.post("/deposit/:contractId", authMiddleware, depositToEscrow);
+router.post("/add-funds", authMiddleware, addFundsToWallet);
+router.post("/withdraw", authMiddleware, withdrawFundsFromWallet);
 
 export default router;
