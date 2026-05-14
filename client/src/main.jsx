ca144e458cdf3c50227b686ecb778f1a70f2fa36
aadi17-arch <facc6982@gmail.com>
2026-05-15 01:39:42 +0530

    harden: client/src/main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { SnackbarProvider } from 'notistack'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <SnackbarProvider 
+        maxSnack={1} 
+        autoHideDuration={2000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        TransitionProps={{ timeout: 0 }}
+      >
+        <App />
+      </SnackbarProvider>
     </AuthProvider>
   </StrictMode>,
 )
+
+
+
+
